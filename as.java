124ffad151dda85e9ca3a49f0811c11eb14f1e43
yogeshrana007 <ranayogesh794@gmail.com>
2024-12-22 10:10:46 +0530

    update bug

--- as.java
+++ as.java
@@ -1,9 +1,11 @@
+
 public class as {
+
     public static void main(String[] args) {
-        StringBuilder sb =new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        System.out.println();
     }
 }
-    
 
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
