2beea55abb4e7047816e93fd3bc22f4dcb02f236
yogeshrana007 <ranayogesh794@gmail.com>
2024-12-23 23:29:45 +0530

    merge,quick,search in rotate and sort

--- DivideConquer/SortAndRotatedArray.java
+++ DivideConquer/SortAndRotatedArray.java
@@ -39,3 +39,38 @@ public class SortAndRotatedArray {
 
     }
 }
+
+//                            <>---------------------<> Agar iterative approach <>------------------->
+/*
+ * public int search(int[] nums, int target) {
+        int si=0;
+        int ei=nums.length-1;
+
+        while(si<=ei){
+
+            int mid=si+(ei-si)/2;
+            
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[si]<=nums[mid]){
+                if(nums[si]<=target && target<=nums[mid]){
+                    ei=mid-1;
+                }
+                else{
+                    si=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && target<=nums[ei]){
+                    si=mid+1;
+                }
+                else{
+                    ei=mid-1;
+                }
+            }
+        }
+        return -1; 
+    }
+}
+ */
