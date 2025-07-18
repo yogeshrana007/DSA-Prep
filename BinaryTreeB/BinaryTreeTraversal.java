edf687d1ac433bd5774ce4c774d027789d107ddb
Yogesh Rana <ranayogesh794@gmail.com>
2025-07-18 12:43:16 +0530

    Binary Tree Traversal methods

---
+++ BinaryTreeB/BinaryTreeTraversal.java
@@ -0,0 +1,126 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTreeTraversal {
+
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+
+        int idx = -1;
+
+        public Node buildTree(int[] nodes) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        // preorder -> root -> left -> right
+        public void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        // inorder ->  left -> root -> right
+        public void inorder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        // postorder ->  left -> right -> root
+        public void postorder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+
+        // LevelOrder (first print level 1 then level 2 -> level 3 ...) O(n)
+        public void levelOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(currNode.data + " ");
+                    if (currNode.left != null) {
+                        q.add(currNode.left);
+                    }
+                    if (currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+
+        Node root = tree.buildTree(nodes);
+
+        // tree is like
+        /*
+         *       1
+         *      / \
+         *     2   3
+         *    / \   \
+         *   4   5   6
+         */
+        // System.out.println(root.data);
+        System.out.print("preorder: ");
+        tree.preorder(root);
+        System.out.print("\ninorder: ");
+        tree.inorder(root);
+        System.out.print("\npostorder: ");
+        tree.postorder(root);
+        System.out.print("\nLevelorder: \n");
+        tree.levelOrder(root);
+    }
+}
