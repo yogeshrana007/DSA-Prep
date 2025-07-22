7e1d42bd5f8ae4f89b51fe9c358aeb29b2bcf35a
Yogesh Rana <ranayogesh794@gmail.com>
2025-07-22 13:27:11 +0530

    Implement Minimum Distance Between Nodes in a Binary Tree

---
+++ BinaryTreeB/MinimumDistanceBetweenNodes.java
@@ -0,0 +1,95 @@
+/*  Approach:-
+ *    1. Find the Lowest common Ancestor (lca)
+ *    2. Then find the distance from lca to nodes
+ *    3. Then add the distances obtained from lca to both nodes, this gives answer
+ */
+package BinaryTreeB;
+
+public class MinimumDistanceBetweenNodes {
+
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca(root.left, n1, n2);
+        Node rightLca = lca(root.right, n1, n2);
+
+        if (rightLca == null) {
+            return leftLca;
+        }
+
+        if (leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
+
+    // distance from lca
+    public static int lcaDist(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if (leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca(root, n1, n2);
+
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        if (dist1 == -1 || dist2 == -1) {
+            System.out.println("One or both node doesn't found!");
+            return -1;
+        }
+        return dist1 + dist2;
+    }
+
+    public static void main(String args[]) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        /*
+         *          1
+         * `       /  \
+         *       2     3
+         *      / \   / \
+         *     4   5 6   7
+         */
+        System.out.println(minDist(root, 4, 7));
+    }
+}
