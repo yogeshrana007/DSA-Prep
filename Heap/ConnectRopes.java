d23b2ce32a5628f92914cc0daed6fa1e319bc8a0
Yogesh Rana <ranayogesh794@gmail.com>
2025-07-30 16:09:24 +0530

    Solve connecting ropes with minimum cost problem

---
+++ Heap/ConnectRopes.java
@@ -0,0 +1,26 @@
+package Heap;
+
+import java.util.PriorityQueue;
+
+public class ConnectRopes {
+
+    public static void main(String args[]) {
+        int[] ropes = {4, 3, 2, 6};
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < ropes.length; i++) {
+            pq.add(ropes[i]);
+        }
+        int cost = 0;
+        while (pq.size() > 1) {
+            int min1 = pq.remove();
+            int min2 = pq.remove();
+
+            cost += (min1 + min2);
+            pq.add(min1 + min2);
+        }
+
+        System.out.println("Minimum cost :" + cost);
+    }
+}
