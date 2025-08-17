3e74ddc82cc30083baf81aa2de045e16990d7c3c
Yogesh Rana <ranayogesh794@gmail.com>
2025-08-17 17:44:27 +0530

    Implement StartsWithProblem using trie data structure for prefix matching

---
+++ Trie/StartsWithProblem.java
@@ -0,0 +1,56 @@
+package Trie;
+
+public class StartsWithProblem {
+
+    static class Node {
+
+        Node[] children = new Node[26];
+        boolean eow = false; // end of word
+
+        public Node() {
+            for (int i = 0; i < children.length; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    // insert in trie
+    public static void insert(String str) {
+
+        Node curr = root;
+
+        for (int i = 0; i < str.length(); i++) {
+            int idx = str.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static boolean startWithPrefix(String key) {
+        Node curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            int idx = key.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String[] words = {"app", "apple", "their", "the", "any"};
+
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+
+        System.out.println(startWithPrefix("i"));
+    }
+}
