6a35bcbbf251ed9e28e1748ace8a18c4b655e366
Yogesh Rana <ranayogesh794@gmail.com>
2025-09-06 15:15:27 +0530

    Update the changes regarding printing duplicates

--- twoDArray/Spiral.java
+++ twoDArray/Spiral.java
@@ -4,21 +4,29 @@ public class Spiral {
     static void spiralMatrix(int matrix[][]){
         int sRow=0;
         int sCol=0;
-        int eRow=matrix.length;
-        int eCol=matrix[0].length;
-        while(sRow<eRow && sCol<eCol ){
+        int eRow=matrix.length-1;
+        int eCol=matrix[0].length-1;
 
-            for(int j=sCol;j<eCol;j++){
+        while(sRow <= eRow && sCol <= eCol ){
+
+            for(int j = sCol; j <= eCol; j++){
                 System.out.print(matrix[sRow][j]+" ");
             }
-            for(int i=sRow+1;i<eRow;i++){
-                System.out.print(matrix[i][eCol-1]+" ");
+
+            for(int i=sRow+1; i <= eRow; i++){
+                System.out.print(matrix[i][eCol]+" ");
             }
-            for(int j=eCol-2;j>=sCol;j--){
-                System.out.print(matrix[eRow-1][j]+" ");
+
+            if(sRow < eRow){
+                for(int j = eCol-1; j >= sCol; j--){
+                    System.out.print(matrix[eRow][j]+" ");
+                }
             }
-            for(int i=eRow-2;i>sRow;i--){
-                System.out.print(matrix[i][sCol]+" ");
+
+            if(sCol < eCol){
+                for(int i=eRow-1; i > sRow; i--){
+                    System.out.print(matrix[i][sCol]+" ");
+                }
             }
             sRow++;
             sCol++;
@@ -28,11 +36,10 @@ public class Spiral {
     }
     
     public static void main(String[] args){
-        int [][] matrix={{1,2,3,4,17},
-                         {5,6,7,8,18},
-                         {9,10,11,12,19},
-                         {13,14,15,16,20},
-                         {21,22,23,24,25}};
+        int [][] matrix={{1,2,3,4},
+                         {5,6,7,8},
+                         {9,10,11,12}
+                        };
 
         spiralMatrix(matrix);
     }   
